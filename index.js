6a4a4d0f3ad5b6d8d960630a8f55fb4470dfd6a3
perry1224 <perry.w1224@gmail.com>
2022-06-19 10:30:09 -0400

    updated users

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/movie_api', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
