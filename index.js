0c351be55c2b283cb8ed7567f791f4a554a428be
perry1224 <perry.w1224@gmail.com>
2022-06-22 11:59:35 -0400

    update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const cors = require('cors');
 app.use(cors());
 
 //mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 let auth = require('./auth')(app);
